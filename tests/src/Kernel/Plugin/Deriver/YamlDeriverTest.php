a9bec246fbda26de68521f10e8f700316682840d
Antonio De Marco <antonio@nuvole.org>
2017-02-21 18:51:19 +0100

    Fix tests #71

--- tests/src/Kernel/Plugin/Deriver/YamlDeriverTest.php
+++ tests/src/Kernel/Plugin/Deriver/YamlDeriverTest.php
@@ -5,6 +5,7 @@ namespace Drupal\Tests\ui_patterns\Kernel\Plugin\Deriver;
 use function bovigo\assert\assert;
 use function bovigo\assert\predicate\hasKey;
 use Drupal\Tests\ui_patterns\Kernel\AbstractUiPatternsTest;
+use Drupal\ui_patterns\UiPatterns;
 
 /**
  * @coversDefaultClass \Drupal\ui_patterns\Plugin\Deriver\YamlDeriver
@@ -14,25 +15,12 @@ use Drupal\Tests\ui_patterns\Kernel\AbstractUiPatternsTest;
 class YamlDeriverTest extends AbstractUiPatternsTest {
 
   /**
-   * Modules to enable.
-   *
-   * @var array
-   */
-  public static $modules = [
-    'ui_patterns',
-    'ui_patterns_test',
-  ];
-
-  /**
    * Test get derivative definitions.
    *
    * @covers ::getDerivativeDefinitions
    */
   public function testGetDerivativeDefinitions() {
-    /** @var \Drupal\ui_patterns\UiPatternsManager $manager */
-    $manager = \Drupal::service('plugin.manager.ui_patterns');
-
-    foreach ($manager->getDefinitions() as $definition) {
+    foreach (UiPatterns::getManager()->getDefinitions() as $definition) {
       assert($definition, hasKey('id')
         ->and(hasKey('provider'))
         ->and(hasKey('base path'))
