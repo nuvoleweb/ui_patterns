6bb1e76aa54c621137cc1b8526b7ff1b406532c2
Antonio De Marco <antonio@nuvole.org>
2017-04-22 10:36:28 +0200

    #91: Remove obsolete test.

--- tests/src/Kernel/UiPatternBaseTest.php
+++ tests/src/Kernel/UiPatternBaseTest.php
@@ -3,7 +3,6 @@
 namespace Drupal\Tests\ui_patterns\Kernel;
 
 use function bovigo\assert\assert;
-use function bovigo\assert\predicate\isTrue;
 use function bovigo\assert\predicate\equals;
 use Drupal\Component\Serialization\Yaml;
 use Drupal\ui_patterns\UiPatternBase;
@@ -14,46 +13,6 @@ use Drupal\ui_patterns\UiPatternBase;
  * @group ui_patterns
  */
 class UiPatternBaseTest extends AbstractUiPatternsTest {
-
-  /**
-   * Test getters.
-   *
-   * @covers ::getId
-   * @covers ::getLabel
-   * @covers ::hasField
-   * @covers ::getField
-   * @covers ::getFieldType
-   * @covers ::getFieldLabel
-   * @covers ::getFieldPreview
-   * @covers ::hasCustomThemeHook
-   * @covers ::getThemeHook
-   * @covers ::getFields
-   * @covers ::getFieldsAsOptions
-   */
-  public function testGetters() {
-    $definitions = $this->getFixtureDefinitions();
-
-    foreach ($definitions as $definition) {
-      /** @var \Drupal\ui_patterns\UiPatternBase $pattern */
-      $pattern = $this->getUiPatternBaseMock($definition);
-
-      assert($pattern->getId(), equals($definition['id']));
-      assert($pattern->getLabel(), equals($definition['label']));
-      assert($pattern->getFields(), equals($definition['fields']));
-
-      foreach ($pattern->getFieldsAsOptions() as $key => $value) {
-        assert($pattern->hasField($key), isTrue());
-        assert($pattern->getFieldLabel($key), equals($value));
-      }
-
-      foreach ($pattern->getFields() as $name => $field) {
-        assert($pattern->getField($name), equals($definition['fields'][$name]));
-        assert($pattern->getFieldType($name), equals($definition['fields'][$name]['type']));
-        assert($pattern->getFieldLabel($name), equals($definition['fields'][$name]['label']));
-        assert($pattern->getFieldPreview($name), equals($definition['fields'][$name]['preview']));
-      }
-    }
-  }
 
   /**
    * Test hookLibraryInfoBuild.
