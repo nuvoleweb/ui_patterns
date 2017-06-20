114e90b220e3c9e2d8150998377a1ef2caf91475
Mike Decker <pookmish@gmail.com>
2017-06-20 16:52:59 -0700

    form alignment to make all forms identical

--- modules/ui_patterns_variants/src/Plugin/Layout/PatternVariants.php
+++ modules/ui_patterns_variants/src/Plugin/Layout/PatternVariants.php
@@ -48,53 +48,12 @@ class PatternVariants extends PatternLayout {
     }
 
     $config = $this->getConfiguration();
-
-    $form['pattern']['variants'] = [
-      '#type' => 'fieldset',
-      '#title' => $this->t('Variants'),
-    ];
-
-    foreach ($variants as $key => $variant) {
-
-      if (!isset($config['pattern']['variants'][$key])) {
-        $config['pattern']['variants'][$key] = [
-          'default' => isset($variant['options']) ? key($variant['options']) : '',
-          'variant_field' => 0,
-          'options' => [],
-        ];
-      }
-
-      $form['pattern']['variants'][$key] = [
-        '#type' => 'fieldset',
-        '#title' => $variant['label'],
-        '#description' => $variant['description'],
-      ];
-
-      // If no variant options are defined, we must rely on field mapping.
-      if (isset($variant['options'])) {
-        $form['pattern']['variants'][$key]['default'] = [
-          '#type' => 'select',
-          '#title' => $this->t('Default value'),
-          '#options' => $variant['options'],
-          '#default_value' => $config['pattern']['variants'][$key]['default'],
-        ];
-
-        $form['pattern']['variants'][$key]['options'] = [
-          '#type' => 'hidden',
-          '#value' => $variant['options'],
-        ];
-      }
-
-      $form['pattern']['variants'][$key]['variant_field'] = [
-        '#type' => 'select',
-        '#title' => $this->t('Variant Field'),
-        '#description' => $this->t('Field to define variant to use.'),
-        '#options' => $field_options,
-        '#empty_option' => $this->t('- None -'),
-        '#default_value' => $config['pattern']['variants'][$key]['variant_field'],
-      ];
+    $defaults = [];
+    if (isset($config['pattern']['variants'])) {
+      $defaults = $config['pattern']['variants'];
     }
 
+    $form['pattern'] += _ui_patterns_variants_get_form_elements($variants, $defaults, $field_options);
     return $form;
   }
 
