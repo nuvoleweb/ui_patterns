5d43ee5f0f0592f8f0bcc601795902f1168fe9f0
Gab <gambrys@gmail.com>
2020-06-25 11:31:05 +0100

    Fix #293: $files null if directory does not exists

--- src/Plugin/Deriver/AbstractYamlPatternsDeriver.php
+++ src/Plugin/Deriver/AbstractYamlPatternsDeriver.php
@@ -62,12 +62,13 @@ abstract class AbstractYamlPatternsDeriver extends AbstractPatternsDeriver imple
     $extensions = $this->getFileExtensions();
     $extensions = array_map('preg_quote', $extensions);
     $extensions = implode('|', $extensions);
+    $files = [];
     if (is_dir($directory)) {
       $files = $this->fileSystem->scanDirectory($directory, "/{$extensions}$/", $options);
+      // In different file systems order of files in a folder can be different
+      // that can break tests. So let's sort them alphabetically manually.
+      ksort($files);
     }
-    // In different file systems order of files in a folder can be different
-    // that can break tests. So let's sort them alphabetically manually.
-    ksort($files);
     return $files;
   }
 
