648c85fa9869fd8c08fbee8c588f50bc234ac9db
Nikolay Lobachev <nikolay.lobachev@gmail.com>
2022-06-30 14:46:15 +0500

    Issue #342: Fix PHP 8.1 compatibility by providing correct return types.

--- src/Definition/ArrayAccessDefinitionTrait.php
+++ src/Definition/ArrayAccessDefinitionTrait.php
@@ -14,28 +14,28 @@ trait ArrayAccessDefinitionTrait {
   /**
    * {@inheritdoc}
    */
-  public function offsetExists($offset) {
+  public function offsetExists($offset): bool {
     return array_key_exists($offset, $this->definition);
   }
 
   /**
    * {@inheritdoc}
    */
-  public function offsetGet($offset) {
+  public function offsetGet($offset): mixed {
     return $this->definition[$offset] ?? NULL;
   }
 
   /**
    * {@inheritdoc}
    */
-  public function offsetSet($offset, $value) {
+  public function offsetSet($offset, $value): void {
     $this->definition[$offset] = $value;
   }
 
   /**
    * {@inheritdoc}
    */
-  public function offsetUnset($offset) {
+  public function offsetUnset($offset): void {
     unset($this->definition[$offset]);
   }
 
